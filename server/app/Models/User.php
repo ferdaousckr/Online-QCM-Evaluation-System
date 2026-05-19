80567da2c36ea26002573b6ed437f90b9a890138
ferdaousckr <chakirferdaous@gmail.com>
2026-05-19 22:32:35 +0100

    chore: resolve merge conflicts

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -16,7 +16,19 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable; 
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role', 
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
     /**
      * Get the attributes that should be cast.
